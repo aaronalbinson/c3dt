f42fb02cec45dc596f3feba655d42f4c4e0866f2
aaronalbinson <azza1990uk@yahoo.com>
2019-12-01 23:14:40 +0000

    #12 Create card component and move content to card

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,31 @@
 <template>
   <div id="app">
-    <SubHeader SubHeaderText="Sub-heading title"/>
-    <MainHeader MainHeaderText="Main title sits here"/>
-    <Paragraph ParagraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non urna eu augue maximus aliquam. Pellentesque sit amet lobortis sapien. Nullam maximus nibh id maximus vestibulum. Donec sit amet fringilla tellus, vel ultrices justo. Phasellus ornare turpis quis augue maximus mattis augue."/>
-    <CallToAction CallToActionText="Call to action" CallToActionLink="https://google.com"/>
-    <C3Carousel/>
+    <Card
+      SubHeaderText="Sub-heading title"
+      MainHeaderText="Main title sits here"
+      ParagraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non urna eu augue maximus aliquam. Pellentesque sit amet lobortis sapien. Nullam maximus nibh id maximus vestibulum. Donec sit amet fringilla tellus, vel ultrices justo. Phasellus ornare turpis quis augue maximus mattis augue."
+      CallToActionText="Call to action"
+      CallToActionLink="https://google.com"
+    />
+    <C3Carousel />
   </div>
 </template>
 
 <script>
-import C3Carousel from './components/C3Carousel/C3Carousel.vue'
-import SubHeader from './components/Headers/SubHeader.vue'
-import MainHeader from './components/Headers/MainHeader.vue'
-import Paragraph from './components/Paragraph/Paragraph.vue'
-import CallToAction from './components/CallToAction/CallToAction.vue'
+import C3Carousel from "./components/C3Carousel/C3Carousel.vue";
+import Card from "./components/Card/Card.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     C3Carousel,
-    SubHeader,
-    MainHeader,
-    Paragraph,
-    CallToAction
+    Card
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 </style>
---
+++ src/components/Card/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="card">
+    <SubHeader SubHeaderText="Sub-heading title"/>
+    <MainHeader MainHeaderText="Main title sits here"/>
+    <Paragraph ParagraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non urna eu augue maximus aliquam. Pellentesque sit amet lobortis sapien. Nullam maximus nibh id maximus vestibulum. Donec sit amet fringilla tellus, vel ultrices justo. Phasellus ornare turpis quis augue maximus mattis augue."/>
+    <CallToAction CallToActionText="Call to action" CallToActionLink="https://google.com"/>
+  </div>
+</template>
+
+<script>
+import SubHeader from '../Headers/SubHeader.vue'
+import MainHeader from '../Headers/MainHeader.vue'
+import Paragraph from '../Paragraph/Paragraph.vue'
+import CallToAction from '../CallToAction/CallToAction.vue'
+
+export default {
+  name: 'Card',
+  props: {
+    SubHeaderText: String,
+    MainHeaderText: String,
+    ParagraphText: String,
+    CallToActionText: String,
+    CallToActionLink: String
+  },
+  components: {
+    SubHeader,
+    MainHeader,
+    Paragraph,
+    CallToAction
+  }
+}
+</script>
+
+<style>
+
+</style>
