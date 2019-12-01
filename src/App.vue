a784cccb98efe2edabfc26800647541a40a9f655
aaronalbinson <azza1990uk@yahoo.com>
2019-12-01 22:31:48 +0000

    #4 Create paragraph component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <SubHeader SubHeaderText="Sub-heading title"/>
     <MainHeader MainHeaderText="Main title sits here"/>
+    <Paragraph ParagraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non urna eu augue maximus aliquam. Pellentesque sit amet lobortis sapien. Nullam maximus nibh id maximus vestibulum. Donec sit amet fringilla tellus, vel ultrices justo. Phasellus ornare turpis quis augue maximus mattis augue."/>
     <C3Carousel/>
   </div>
 </template>
@@ -10,13 +11,15 @@
 import C3Carousel from './components/C3Carousel/C3Carousel.vue'
 import SubHeader from './components/Headers/SubHeader.vue'
 import MainHeader from './components/Headers/MainHeader.vue'
+import Paragraph from './components/Paragraph/Paragraph.vue'
 
 export default {
   name: 'app',
   components: {
     C3Carousel,
     SubHeader,
-    MainHeader
+    MainHeader,
+    Paragraph
   }
 }
 </script>
---
+++ src/components/Paragraph/Paragraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="paragraph">{{ ParagraphText }}</p>
+</template>
+
+<script>
+export default {
+  name: "Paragraph",
+  props: {
+    ParagraphText: String
+  },
+};
+</script>
+
+<style scoped>
+    .paragraph {
+        text-align: left;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
