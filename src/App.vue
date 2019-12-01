3686c14efbefc6c6ed05634d2a9a88f7253a1fc2
aaronalbinson <azza1990uk@yahoo.com>
2019-12-01 22:36:53 +0000

    #5 Add call to action component

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <SubHeader SubHeaderText="Sub-heading title"/>
     <MainHeader MainHeaderText="Main title sits here"/>
     <Paragraph ParagraphText="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non urna eu augue maximus aliquam. Pellentesque sit amet lobortis sapien. Nullam maximus nibh id maximus vestibulum. Donec sit amet fringilla tellus, vel ultrices justo. Phasellus ornare turpis quis augue maximus mattis augue."/>
+    <CallToAction CallToActionText="Call to action" CallToActionLink="https://google.com"/>
     <C3Carousel/>
   </div>
 </template>
@@ -12,6 +13,7 @@ import C3Carousel from './components/C3Carousel/C3Carousel.vue'
 import SubHeader from './components/Headers/SubHeader.vue'
 import MainHeader from './components/Headers/MainHeader.vue'
 import Paragraph from './components/Paragraph/Paragraph.vue'
+import CallToAction from './components/CallToAction/CallToAction.vue'
 
 export default {
   name: 'app',
@@ -19,7 +21,8 @@ export default {
     C3Carousel,
     SubHeader,
     MainHeader,
-    Paragraph
+    Paragraph,
+    CallToAction
   }
 }
 </script>
---
+++ src/components/CallToAction/CallToAction.vue
@@ -0,0 +1,20 @@
+<template>
+  <a class="callToAction" :href="CallToActionLink">{{ CallToActionText }}</a>
+</template>
+
+<script>
+export default {
+  name: "CallToAction",
+  props: {
+    CallToActionText: String,
+    CallToActionLink: String
+  },
+};
+</script>
+
+<style scoped>
+    .callToAction {
+        text-align: left;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
